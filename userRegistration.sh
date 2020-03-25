9525efb1887d14459c35b0744695a9f559e33eb7
Saksham27 <geekysaksham27@gmail.com>
2020-03-25 17:52:05 +0530

    Added: taking full name and function to validate first name and last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,23 @@
 #!/bin/bash -x
 
-# taking first name as input
-read -p "First Name : " firstName
-firstNameRegex="([A-Z]{1}[a-z]{2})"
-if [[ $firstName =~ $firstNameRegex ]]
-then
-	echo "yes"
-else
-	echo "no"
-fi
+# taking name as input
+read -p "First Name : " name
+
+
+# function to validate name
+# param1 : Name
+function validateName() {
+	firstName=`echo $1 | awk -F" " '{print $1}'`
+	lastName=`echo $1 | awk -F" " '{print $2}'`
+
+	nameRegex="([A-Z]{1}[a-z]{2})"
+
+	if [[ $firstName =~ $nameRegex && $lastName =~ $nameRegex ]]
+	then
+		echo "Name is valid."
+	else
+		echo "Invalid Name."
+	fi
+}
+
+validateName "$name"
